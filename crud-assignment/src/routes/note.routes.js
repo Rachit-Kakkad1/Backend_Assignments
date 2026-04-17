fbb1589b612de7ee32b5f7114c9c8a21117f138e
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-04-17 16:29:19 +0530

    feat: add POST /bulk route for bulk note creation

--- crud-assignment/src/routes/note.routes.js
+++ crud-assignment/src/routes/note.routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 import {
   createNote,
+  createNotesBulk,
 } from "../controllers/note.controller.js";
 
 router.post("/", createNote);
+router.post("/bulk", createNotesBulk);
 
 
 export default router;
