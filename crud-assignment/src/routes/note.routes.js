fce761579185db3e5a867ab7a9747ba208a58fbe
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-04-17 16:34:31 +0530

    feat: add GET /:id route for fetching a single note

--- crud-assignment/src/routes/note.routes.js
+++ crud-assignment/src/routes/note.routes.js
@@ -4,12 +4,14 @@ const router = express.Router();
 import {
   createNote,
   createNotesBulk,
-  getAllNotes
+  getAllNotes,
+  getNoteById
 } from "../controllers/note.controller.js";
 
 router.post("/", createNote);
 router.post("/bulk", createNotesBulk);
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
 
 
 export default router;
