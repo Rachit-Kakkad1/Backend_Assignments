f66442f4921bb3347d7269e5a57c0e233d82f216
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-04-17 16:32:58 +0530

    feat: add GET / route for fetching all notes

--- crud-assignment/src/routes/note.routes.js
+++ crud-assignment/src/routes/note.routes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 import {
   createNote,
   createNotesBulk,
+  getAllNotes
 } from "../controllers/note.controller.js";
 
 router.post("/", createNote);
 router.post("/bulk", createNotesBulk);
+router.get("/", getAllNotes);
 
 
 export default router;
