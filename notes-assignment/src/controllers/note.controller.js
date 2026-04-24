4e17559fef72aee67e732bde88ebb96bb2bfcf9f
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-04-24 09:38:46 +0530

    chore: initial project setup with dependencies

---
+++ notes-assignment/src/controllers/note.controller.js
@@ -0,0 +1,37 @@
+import Note from "../models/note.model.js";
+
+// @desc    Create a single note
+// @route   POST /api/notes
+// @access  Public
+export const createNote = async (req, res) => {
+  try {
+    const { title, content, category, isPinned } = req.body;
+
+    if (!title || !content) {
+      return res.status(400).json({
+        success: false,
+        message: "Title and content are required",
+        data: null,
+      });
+    }
+
+    const note = await Note.create({
+      title,
+      content,
+      category,
+      isPinned,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Note created successfully",
+      data: note,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
